bdcc90acab9103bd4fd046463dcfc96c8c0d1f09
Chris Mendez <ttrojan@ttrojan.local>
2015-04-28 21:55:58 -0700

    added concurrency examples

---
+++ Examples of Concurrency/DispatchQueues.playground/section-1.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+/** ** ** ** ** ** ** ** ** ** ** ** ** ** **
+TITLE: Dispatch Queues
+
+
+DESCRIPTION: 
+
+There are TWO types:
+• Sequential (series)
+• Concurrent (parallel)
+
+OTHER NOTES:
+
+
+OLD WAY:
+Objective-C thinks about the world using the idea of BLOCKS
+
+
+SWIFT WAY:
+SWIFT thinks about the world using the idea of CLOSURES
+
+** ** ** ** ** ** ** ** ** ** ** ** ** ** **/
+
+//EXAMPLE 6: SERIAL QUEUE
+//You have to create SERIAL queue's before you can create them.
+
+//Declare a variable to hold a reference to a dispatch queue = then create the queue, "name", serial CONSTANT
+var q:dispatch_queue_t = dispatch_queue_create("fm.guitarpick.queue", DISPATCH_QUEUE_SERIAL)
+
+dispatch_async(q, {
+    /*
+    Do some work
+    */
+})
+
+
+//EXAMPLE 7: CONCURRENT QUEUE
+
+//
+var d:dispatch_queue_t = dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0 )
+
+dispatch_async(d, {
+    /*
+    Do some work
+    */
+    dispatch_async( dispatch_get_main_queue() ){
+        
+    }
+})
