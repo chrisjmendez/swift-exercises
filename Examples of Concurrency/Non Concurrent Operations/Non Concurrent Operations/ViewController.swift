d415f0782329c185ad1ebebc641a74aa59277c29
Chris Mendez <ttrojan@ttrojan.local>
2015-05-04 09:51:32 -0700

    New Project - Non-concurrent operations

---
+++ Examples of Concurrency/Non Concurrent Operations/Non Concurrent Operations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Non Concurrent Operations
+//
+//  Created by tommy trojan on 5/4/15.
+//  Copyright (c) 2015 Skyground Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Examples of Concurrency/Non Concurrent Operations/Non Concurrent OperationsTests/Non_Concurrent_OperationsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Non_Concurrent_OperationsTests.swift
+//  Non Concurrent OperationsTests
+//
+//  Created by tommy trojan on 5/4/15.
+//  Copyright (c) 2015 Skyground Media. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Non_Concurrent_OperationsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
