167d05ee2cd859b29e3d7beb3bf42fc1d673e2fd
Chris Mendez <ttrojan@ttrojan.local>
2015-05-07 12:41:38 -0700

    Facebook Login

---
+++ Social/Facebook/Facebook/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  FacebookTutorial
+//
+//  Created by Brian Coleman on 2015-03-27.
+//  Copyright (c) 2015 Brian Coleman. All rights reserved.
+//
+
+#ifndef FacebookTutorial_Bridging_Header_h
+#define FacebookTutorial_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
