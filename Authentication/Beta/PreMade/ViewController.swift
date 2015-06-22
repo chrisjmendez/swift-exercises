7f8240e2674c5a73ac6448d3ddeef0892f437fcb
chrisjmendez <mail@chrisaiv.com>
2015-06-22 13:21:24 -0700

    Completed PreMade example

--- Authentication/Beta/PreMade/ViewController.swift
+++ Authentication/Beta/PreMade/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
+extension ViewController: PFSignUpViewControllerDelegate {
     override func viewDidAppear(animated: Bool) {
         loadBoilerplate()
     }
@@ -51,5 +51,58 @@ extension ViewController: PFLogInViewControllerDelegate, PFSignUpViewControllerD
         } else {
            statusLbl.text = "Welcome \(PFUser.currentUser()?.username)"
         }
+    }
+    
+    func signUpViewController(signUpController: PFSignUpViewController, shouldBeginSignUp info: [NSObject : AnyObject]) -> Bool {
+        var informationComplete = true
+
+        println(info)
+        /*
+        for key in info {
+            var field:NSString = info[key]
+            if( !field || !field.length){
+                informationComplete = false
+                break
+            }
+        }
+        */
+        return true
+    }
+    
+    
+    func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func signUpViewController(signUpController: PFSignUpViewController, didFailToSignUpWithError error: NSError?) {
+        println("Failed to sign up ")
+    }
+    
+    func signUpViewControllerDidCancelSignUp(signUpController: PFSignUpViewController) {
+        println("User dismissed the signupViewController")
+    }
+}
+
+extension ViewController: PFLogInViewControllerDelegate{
+    
+    func logInViewController(logInController: PFLogInViewController, shouldBeginLogInWithUsername username: String, password: String) -> Bool {
+        if String(username) != nil && String(password) != nil && count(username) > 0  && count(password) > 0 {
+                return true
+        }
+        println("Missing information")
+        return false
+    }
+    
+    func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        print("User has logged in")
+    }
+    
+    func logInViewController(logInController: PFLogInViewController, didFailToLogInWithError error: NSError?) {
+        println("Failed to Log in")
+    }
+    
+    func logInViewControllerDidCancelLogIn(logInController: PFLogInViewController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 }
