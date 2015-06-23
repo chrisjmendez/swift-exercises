37336b2aa36c2edb7afd01b662cd40eb1dbd6ae1
chrisjmendez <mail@chrisaiv.com>
2015-06-23 08:11:37 -0700

    refactored

---
+++ Authentication/Parse/C - Custom Login/CustomLogin/com/chrisjmendez/auth/AppLogInViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AppLogInViewController.swift
+//  CustomLogin
+//
+//  Created by tommy trojan on 6/23/15.
+//  Copyright (c) 2015 Chris Mendez. All rights reserved.
+//
+
+import Foundation
