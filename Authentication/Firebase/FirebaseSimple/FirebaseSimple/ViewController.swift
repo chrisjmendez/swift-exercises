caedf276195421c345c19a35dc514f298a514a18
Chris Mendez <signup@chrisjmendez.com>
2015-09-27 12:53:40 -0700

    Simple Firebase

--- Authentication/Firebase/FirebaseSimple/FirebaseSimple/ViewController.swift
+++ Authentication/Firebase/FirebaseSimple/FirebaseSimple/ViewController.swift
@@ -11,29 +11,24 @@ import UIKit
 class ViewController: UIViewController {
 
     var db = Firebase(url: "https://swift-example.firebaseio.com/")
-    var message = "WE JUST CHANGED THE VALUE"
-    
-    @IBOutlet weak var dataLabel: UILabel!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let newClient = "USC Radio Network"
+        let subsidary = ["Classical KUSC", "Classical KDFC"]
+        
+        db.childByAppendingPath(newClient).setValue(newClient)
+        db.childByAppendingPath(newClient).childByAppendingPath(subsidary[0]).setValue("Los Angeles")
+        db.childByAppendingPath(newClient).childByAppendingPath(subsidary[1]).setValue("San Francisco")
         
         //Pull the value from Firebase using the Observer Pattern
         db.observeEventType(.Value, withBlock: {
             //Get the Snapshot of the code and print a value
             snapshot in
-            self.dataLabel.text = snapshot.value as? String
+            var value = snapshot.value.objectForKey(newClient)
+            println(value)
         })
-        
-        //Write to Firebase
-//        db.setValue(message)
-        
-        // ////////////////////////////////
-        //Example 2
-        // ////////////////////////////////
-        
         
     }
 
