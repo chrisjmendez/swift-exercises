1e54c95fedae4bd14eb4100369a6b248a66a1a50
Chris Mendez <signup@chrisjmendez.com>
2015-11-21 10:27:32 -0800

    Importing Facebook Audience Network Example
    Basic example of how to hook your app to FB audience network

---
+++ Advertising/FBAudienceNetwork/FBAudienceNetwork/Config.swift
@@ -0,0 +1,17 @@
+//
+//  Config.swift
+//  FBAudienceNetwork
+//
+//  Created by Tommy Trojan on 11/21/15.
+//  Copyright © 2015 Chris Mendez. All rights reserved.
+//
+
+class Config {
+    
+    struct permissions {
+        static let name          = "name"
+        static let email         = "email"
+        static let publicProfile = "public_profile"
+        static let userFriends   = "user_friends"
+    }
+}
