318a7cc4455478d1778c5976310a89e5caf5587a
Chris Mendez <signup@chrisjmendez.com>
2016-01-04 05:26:13 -0800

    removed /UIX added new demo /FeaturedCollection

---
+++ GUI/FeaturedCollection/FeaturedCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FeaturedCollection
+//
+//  Created by Tommy Trojan on 1/4/16.
+//  Copyright © 2016 Chris Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
