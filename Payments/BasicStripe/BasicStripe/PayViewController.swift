1599a363135226f26c1f2880257b4203f8b396bf
Chris Mendez <signup@chrisjmendez.com>
2016-01-13 14:52:51 -0800

    updatd sprite payments

--- Payments/BasicStripe/BasicStripe/PayViewController.swift
+++ Payments/BasicStripe/BasicStripe/PayViewController.swift
@@ -18,7 +18,55 @@ class PayViewController: UIViewController {
     @IBOutlet weak var cvv: TextField!
     
     @IBAction func onPay(sender: AnyObject) {
+        validateAndPay()
+    }
+    
+    func validateAndPay(){
+        let creditCard = STPCardParams()
+        creditCard.number = number.text
+        creditCard.cvc    = cvv.text
         
+        if(((expiration.text?.isEmpty) == nil)){
+            let expArr = expiration.text?.componentsSeparatedByString("/")
+            if expArr?.count > 1 {
+                
+                let mm:NSNumber = Int(expArr![0])!
+                let yy:NSNumber = Int(expArr![1])!
+                
+                creditCard.expMonth = mm.unsignedLongValue
+                creditCard.expYear  = yy.unsignedLongValue
+            }
+        }
+        
+        do {
+            try creditCard.validateCardReturningError()
+            /*
+            STPAPIClient.sharedClient().createTokenWithCard(
+                creditCard,
+                completion: { (token: STPToken?, stripeError: NSError?) -> Void in
+                    self.createBackendChargeWithToken(token!, completion: completion)
+            })
+            */
+            print("validated")
+
+        } catch {
+            showAlert()
+        }
+    }
+    
+    func showAlert(){
+        //Create the AlertController
+        let actionSheetController: UIAlertController = UIAlertController(title: "Payment Error", message:
+            "Please enter a valid credit card.", preferredStyle: .Alert)
+        
+        //Create and add the Cancel action
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
+            //Do some stuff
+        }
+        actionSheetController.addAction(cancelAction)
+        
+        //Present the AlertController
+        self.presentViewController(actionSheetController, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
