128a92cdaec74469a22e803b374d2f44cd4d9f27
Chris Mendez <signup@chrisjmendez.com>
2016-01-13 14:32:19 -0800

    New Stripe Demo

---
+++ Payments/BasicStripe/BasicStripe/TextField.swift
@@ -0,0 +1,49 @@
+//
+//  TextField.swift
+//  CustomForm
+//
+//  Created by Tommy Trojan on 12/21/15.
+//  Copyright © 2015 Chris Mendez. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class TextField: UITextField {
+    
+    @IBInspectable var insetX: CGFloat = 0
+    @IBInspectable var insetY: CGFloat = 0
+    
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clearColor() {
+        didSet {
+            layer.borderColor = borderColor.CGColor
+        }
+    }
+    
+    @IBInspectable var borderWidth:CGFloat = 0 {
+        didSet{
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    // placeholder position
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds , insetX , insetY)
+    }
+    
+    // text position
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds , insetX , insetY)
+    }
+    
+    override func placeholderRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, insetX, insetY)
+    }
+        
+}
