24093faeddb0cb7b9b94dd582a913d36d7accba6
Chris Mendez <mail@chrisaiv.com>
2016-01-19 16:14:00 -0800

    Music Player transition

---
+++ Transitions/MusicPlayerTransition/MusicPlayerTransition/ModalViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ModalViewController.swift
+//  SoundCloudTransition
+//
+//  Created by xxxAIRINxxx on 2015/02/25.
+//  Copyright (c) 2015 xxxAIRINxxx. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+    
+    @IBOutlet weak var imageView : UIImageView!
+    
+    var tapCloseButtonActionHandler : (Void -> Void)?
+    
+    @IBAction func tapCloseButton() {
+        self.tapCloseButtonActionHandler?()
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ModalViewController viewWillAppear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("ModalViewController viewWillDisappear")
+    }
+}
