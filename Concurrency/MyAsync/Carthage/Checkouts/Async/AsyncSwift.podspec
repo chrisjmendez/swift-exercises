c08e6b62f3bf796341bcac8baa51cdedfbfaaf8b
Chris Mendez <cmendez@silverbullet.local>
2016-02-27 12:30:22 -0800

    Created a new demo for Async and refactored the files in GUI

---
+++ Concurrency/MyAsync/Carthage/Checkouts/Async/AsyncSwift.podspec
@@ -0,0 +1,16 @@
+Pod::Spec.new do |s|
+  s.name        = "AsyncSwift"
+  s.version     = "1.7.2"
+  s.summary     = "Syntactic sugar in Swift for asynchronous dispatches in Grand Central Dispatch"
+  s.homepage    = "https://github.com/duemunk/Async"
+  s.license     = { :type => "MIT" }
+  s.authors     = { "Tobias Due Munk" => "tobias@developmunk.dk" }
+
+  s.osx.deployment_target = "10.10"
+  s.ios.deployment_target = "8.0"
+  s.tvos.deployment_target = "9.0"
+  s.source   = { :git => "https://github.com/duemunk/Async.git", :tag => "1.7.2"}
+  s.source_files = "Source/*.swift"
+  s.requires_arc = true
+  s.module_name = 'Async'
+end
--- Concurrency/MyAsync/MyAsync/ViewController.swift
+++ Concurrency/MyAsync/MyAsync/ViewController.swift
@@ -7,19 +7,160 @@
 //
 
 import UIKit
+import Async
+import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
 
+    let CUSTOMER_NAMES = "http://codepen.io/chrisaiv/pen/NNWvBO.js"
+    
+    //MARK: - Outlets
+    @IBOutlet weak var nextBuyer: UILabel!
+    
+    @IBOutlet weak var currentBuyer: UILabel!
+    
+    @IBOutlet weak var tapeRoll: UITextView!
+    
+    @IBOutlet weak var alphaSlider: UISlider!
+    
+    @IBAction func onReset(sender: AnyObject) {
+        clearLabels()
+    }
+    
+    @IBOutlet weak var buyBtn: UIButton!
+    @IBAction func onBuy(sender: AnyObject) {
+        buyTicket()
+        disableBuy()
+    }
+    
+    var customers:[String] = []
+    var currentIdx:Int = 0
+    
+    //MARK: - GUI
+    func disableBuy(){
+        buyBtn.enabled = false
+    }
+    
+    func enableBuy(){
+        buyBtn.enabled = true
+    }
+    
+    func clearLabels(){
+        updateTapeRoll("---")
+        updateNextBuyer("---")
+        updateCurrentBuyer("---")
+    }
+    
+    func updateTapeRoll(string:String){
+        let oldMessage:String = tapeRoll.text
+        let newMessage:String = "\(string)\n"
+        
+        tapeRoll.text = oldMessage + newMessage
+    }
+    
+    func updateCurrentBuyer(string:String){
+        print("updateCurrentBuyer: ", string)
+        currentBuyer.text = string
+    }
+    
+    func updateNextBuyer(string:String){
+        print("updateNextBuyer:", string)
+        nextBuyer.text = string
+    }
+    
+    //Get JSON data
+    func queueCustomers(){
+        Alamofire.request(.GET, CUSTOMER_NAMES).responseJSON { (response) -> Void in
+            if response.result.isFailure {
+                print("Error")
+            } else {
+                let data = JSON(response.result.value!)
+                //Append each JSON object item into an NSArray
+                for(index, value):(String, JSON) in data {
+                    let name = value["author"].string!
+                    self.customers.append( name )
+                }
+                self.updateTapeRoll("Customer Data Loaded")
+            }
+        }
+    }
+    
+    //MARK: - Customer buys and pays for ticket
+    func buyTicket(){
+        
+        print("Customer:", currentIdx ,"of:", customers.count)
+        
+        if currentIdx >= customers.count{
+            print("z")
+            clearLabels()
+            disableBuy()
+            updateCurrentBuyer("No More Customer")
+        } else {
+            let thisCustomer = self.customers[self.currentIdx]
+            let nextCustomer = self.customers[self.currentIdx + 1]
+
+            var timeFrame:Double = 0.0
+
+            let parallelQueue = dispatch_queue_create("SerialQueue", DISPATCH_QUEUE_CONCURRENT)
+            
+            Async.main {
+                print("a")
+                //Indicate who is currently buying and who is coming up next
+                self.updateCurrentBuyer("Current: \(thisCustomer)")
+                self.updateNextBuyer("Next: \(nextCustomer)")
+            }.background {
+                print("b0")
+
+                //Retreive data from 3 different processes and wait for it to happen
+                let group = AsyncGroup()
+                    group.background {
+                        print("b1", "Waiting to secure ticket")
+                        timeFrame = Simulator.sharedInstance.runSimulatorWithMinTime(1, maxTime: 5)
+                    }
+                    group.background {
+                        print("b2", "Securing payment tocken")
+                        timeFrame = Simulator.sharedInstance.runSimulatorWithMinTime(1, maxTime: 5)
+                    }
+                    group.background {
+                        print("b3", "Submitting payment")
+                        self.submitPayment(thisCustomer)
+                    }
+                    group.wait()
+            }.main {
+                print("d0", "A Processes were completed")
+                self.updateTapeRoll("\(thisCustomer) bought tickets in \(timeFrame).")
+                print("d1")
+                self.currentIdx++
+                self.enableBuy()
+            }
+        }
+    }
+    
+    func submitPayment(customerName:String){
+        print("c0")
+        var duration:Double = 0.0
+        let group = AsyncGroup()
+
+        group.background {
+            print("c1")
+            duration = Simulator.sharedInstance.runSimulatorWithMinTime(2, maxTime: 5)
+            print("Payment sucess in \(duration)")
+        }
+        group.main {
+            print("c2")
+            print("Payment completed")
+            self.updateTapeRoll("\(customerName) paid in \(duration)")
+        }
+        group.wait()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        clearLabels()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(animated: Bool) {
+        queueCustomers()
     }
-
-
 }
-
